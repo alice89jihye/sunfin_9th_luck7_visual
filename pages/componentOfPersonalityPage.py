29c657c290182f1e16fc7243c9a6e97ce9e2806e
jihye <jihye@ididinc.com>
2024-08-27 20:41:31 +0900

    font 수정

--- pages/componentOfPersonalityPage.py
+++ pages/componentOfPersonalityPage.py
@@ -7,14 +7,14 @@ def show_content():
     divider.space(40)
 
     customText.boldNoLink("What is PERSONALITY?")
-    customText.regularNoLink("Allport, G. W. (1937). Personality: a psychological interpretation. Holt.", None, "16px", False)
-    customText.link("https://archive.org/details/in.ernet.dli.2015.155561/page/n45/mode/2up", "https://archive.org/details/in.ernet.dli.2015.155561/page/n45/mode/2up", "#0000FF", "16px", "0px")
+    customText.regularNoLink("Allport, G. W. (1937). Personality: a psychological interpretation. Holt.")
+    customText.link("https://archive.org/details/in.ernet.dli.2015.155561/page/n45/mode/2up", "https://archive.org/details/in.ernet.dli.2015.155561/page/n45/mode/2up")
     divider.space(40)
 
     customText.mixed_style_text("“Persona”","_ the theatrical mask first used in Greek drama")
-    customText.italicNoLink("peri sô ma (around the body)", None, "16px", True)
-    customText.italicNoLink("persum (head or face; the Etruscan and Old Latin).", None, "16px", True)
-    customText.italicNoLink("per se una (self-containing; the Latin).®", None, "16px", True)
+    customText.italicNoLink("peri sô ma (around the body)")
+    customText.italicNoLink("persum (head or face; the Etruscan and Old Latin).")
+    customText.italicNoLink("per se una (self-containing; the Latin).®")
     divider.space(40)
 
 
